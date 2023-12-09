6af8b4d93e87cd2b63d52118c0037cb20af6d06b
Alex Fomichev <alex.fomichev.1990@gmail.com>
2023-12-09 20:59:28 +0600

    view files added

---
+++ resources/views/main-form.blade.php
@@ -0,0 +1,21 @@
+<div class="container col-xl-10 col-xxl-8 px-4 py-5">
+   <div class="row align-items-center g-lg-5 py-5">
+   <div class="col-lg-7 text-center text-lg-start">
+      <h1 class="display-4 fw-bold lh-1 mb-3">Paste URL and get short link</h1>
+      <p class="col-lg-10 fs-4">Recently added</p>
+      <div>
+         <ul id="last-items"></ul>
+      </div>
+   </div>
+   <div class="col-md-10 mx-auto col-lg-5">
+      <form class="p-4 p-md-5 border rounded-3 bg-light">
+         <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="link">
+            <label for="link">Paste URL here</label>
+         </div>
+
+         <button class="w-100 btn btn-lg btn-primary" type="submit" id="send">Shorten.it!</button>
+      </form>
+   </div>
+   </div>
+</div>
